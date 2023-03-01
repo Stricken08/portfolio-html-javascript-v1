7e3139f669702d1847a4ac5006299b33ae4bf09b
Stricken08 <axel.bacquet06@gmail.com>
2023-03-01 12:30:58 +0100

    testfooter

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+
 const scroll = new LocomotiveScroll({
   el: document.querySelector("[data-scroll-container]"),
   smooth: true,
@@ -15,3 +16,111 @@ scroll.on("scroll", () => {
     document.body.style.color = "#000101";
   }
 });
+
+//////////////////////////////////////////////////////
+window.onload = function(){
+	
+  console.log('Made by Axel Bacquet ;) - A Passionate Web Developer!');
+  
+  window.addEventListener("scroll", updateYPosition, false);
+  window.addEventListener("resize", updateYPosition, false);
+  
+  navEffect();
+  updateYPosition();
+  selectModal(marineButton, marineModal);
+  selectModal(aguaButton, aguaModal);
+  selectModal(todoButton, todoModal);
+  animateHTML().init();
+  whenUserScroll().init();
+  
+};
+
+//VARIABLE CONTAINING ALL ANIMATIONS 
+var animateHTML = function() {
+var leftElems;
+var rightElems;
+var botElems;
+var barElems;
+var bar60Elems;
+var bar90Elems;
+var windowHeight;
+//USE THIS TO INITIALIZE ALL FUNCTIONS IN THIS SCOPE
+function init() {
+leftElems = document.querySelectorAll('.hidden');
+rightElems = document.querySelectorAll('.hiddenRight');
+botElems = document.querySelectorAll('.hiddenBot');
+barElems = document.querySelectorAll('.fill-80-bar');
+bar60Elems = document.querySelectorAll('.fill-60-bar');
+bar90Elems = document.querySelectorAll('.fill-90-bar');
+windowHeight = window.innerHeight;
+addEventHandlers();
+
+}
+
+function addEventHandlers() {
+/*ADDING EVENT LISTENER FOR FADE IN ANIMATIONS*/
+window.addEventListener('scroll', function(){ checkAnyPosition(leftElems, 'hidden', 'slideLeft'); });
+window.addEventListener('scroll', function(){ checkAnyPosition(rightElems, 'hiddenRight', 'slideRight'); });
+window.addEventListener('scroll', function(){ checkAnyPosition(botElems, 'hiddenBot', 'slideBot'); });
+/*ADDING EVENT LISTENERS FOR FILLING BAR ANIMATIONS*/
+window.addEventListener('scroll',function(){ fillingAllBars(barElems, '80'); });
+window.addEventListener('scroll',function(){ fillingAllBars(bar60Elems, '60'); });
+window.addEventListener('scroll',function(){ fillingAllBars(bar90Elems, '90'); });
+window.addEventListener('resize', init);
+
+}
+
+/***********STARTS FADE IN ANIMATIONS******************/	  
+function checkAnyPosition(elem, fromPos, toPos) {
+for (var i = 0; i < elem.length; i++) {
+  var positionFromTop = elem[i].getBoundingClientRect().top;
+  if (positionFromTop - windowHeight <= 0) {
+    elem[i].className = elem[i].className.replace(
+      fromPos,
+      toPos
+    );
+  }
+}
+}
+/**************Starts FILLING BARS SECTION*******************/
+function fillingAllBars(elem, size) {
+for (var i = 0; i < elem.length; i++) {
+  var positionFromTop = elem[i].getBoundingClientRect().top;
+  if (positionFromTop - windowHeight <= 0) {
+    elem[i].className = elem[i].className.replace(
+      'fill-'+size+'-bar',
+      'filed-'+size+'-bar'
+      );
+  }
+}
+}
+return {
+init: init
+};
+};
+
+
+//NAV FADE_TOP_TO_BOTTOM EFFECT
+function navEffect() {
+      window.addEventListener("scroll", function(){
+      var navBar = document.getElementById('navbar');
+      var domRect = navBar.getBoundingClientRect();
+      var myBackground = document.getElementById('my-background');
+      var domBGRect = myBackground.getBoundingClientRect();
+      
+      if(domRect.y <= -domRect.height){
+          navBar.classList.add('fade-in-nav');
+      }
+      if(-domBGRect.height < domBGRect.top){
+          navBar.classList.remove('fade-in-nav');
+      }
+  });
+  
+}
+
+function updateYPosition(elem) {
+var yScroll = window.pageYOffset;
+//console.log(yScroll);
+
+return elem;
+}
